c79c3c5c63dbe2380371e7413beccbba796199a6
Abi D <axa.abigail.i@gmail.com>
2019-10-04 20:26:33 -0500

    started adding save page functionality to index.js

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ var saved = [];
 /* HOME PAGE FUNCTIONALITY */
 
 // get all of the data from our articles route
-$.get('/articles', function(data){
+$.get('/scrape', function(data){
     // loop over data array to append to #scrapes column
     for (var i = 0; i < data.length; i++) {
         $('#scrapes').append(`<div class="card mb-3">
@@ -33,6 +33,26 @@ $(document).on('click', '#newArticles', function(){
 // button click event for saving an article
 $(document).on('click', '#save', function(){
     console.log($(this));
+});
+
+/* SAVED PAGE FUNCTIONALITY */
+
+$.get('/articles', function(data){
+    for (let i=0; i < data.length; i++) {
+        $('#savedArticles').append(`<div class="card mb-3">
+        <div class="row no-gutters">
+        <div class="col-md-4 scrape-image" style="background-image:url('${data[i].image}');">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+            <h5 class="card-title">${data[i].title}</h5>
+            <p class="card-text">${data[i].category}</p>
+            <button class="btn btn-primary comment">Comment</button> <button class="btn btn-secondary commentdelete" id="${data[i]._id}">X</button>
+            </div>
+        </div>
+        </div>
+    </div>`)
+    }
 });
 
 });
